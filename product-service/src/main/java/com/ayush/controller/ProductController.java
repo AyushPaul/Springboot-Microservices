33cb1ae066509c47c553b1742607b74beb20345d
Ayush Paul <ayushpaul2002@gmail.com>
2023-04-26 23:12:12 +0530

    Implemented Basic Product Service

---
+++ product-service/src/main/java/com/ayush/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.ayush.controller;
+
+import com.ayush.dto.ProductRequest;
+import com.ayush.dto.ProductResponse;
+import com.ayush.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    private final ProductService productService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest) {
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
+    }
+}
